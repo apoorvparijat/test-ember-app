1eefd06c828e3d1cf3a0fa2a2c5cd65d0c669ccc
Apoorv Parijat <apoorv.parijat@intercom.io>
2019-12-18 16:10:33 +0000

    With willtransition in contact

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    willTransition(transition) {
+      if(!confirm('From contact: Are you sure you want to abandon progress?')) {
+        transition.abort();
+      }
+    }
+  }
 });
