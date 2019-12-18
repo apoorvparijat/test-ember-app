1f114948898e18f9f93239be5546bec2e1007088
Apoorv Parijat <apoorv.parijat@intercom.io>
2019-12-18 15:52:33 +0000

    Ember test app with transition.abort

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    willTransition(transition) {
+      if(!confirm('Are you sure you want to abandon progress?')) {
+        transition.abort();
+      }
+    }
+  }
 });
