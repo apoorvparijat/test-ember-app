3238c68ff8f3234ec9fc616953a52211d74c653f
Apoorv Parijat <apoorv.parijat@intercom.io>
2019-12-18 16:21:46 +0000

    Final code

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -5,6 +5,10 @@ export default Route.extend({
     willTransition(transition) {
       if(!confirm('From contact: Are you sure you want to abandon progress?')) {
         transition.abort();
+      } else {
+        // Bubble the `willTransition` action so that
+        // parent routes can decide whether or not to abort.
+        return true;
       }
     }
   }
